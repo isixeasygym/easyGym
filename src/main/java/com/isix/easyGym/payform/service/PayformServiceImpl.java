ebb0bee64d52866a1cb02a789c192c51e143fef3
2jang <jakeecc@naver.com>
2024-07-12 18:21:45 +0900

    페이폼 파일 추가 및 이름 변경

--- src/main/java/com/isix/easyGym/payform/service/PayformServiceImpl.java
+++ src/main/java/com/isix/easyGym/payform/service/PayformServiceImpl.java
@@ -1,15 +1,24 @@
 package com.isix.easyGym.payform.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.isix.easyGym.payform.dao.PayformDAO;
+
+import java.util.Map;
 
 @Service("payformService")
 public class PayformServiceImpl implements PayformService {
 
 	@Autowired
 	private PayformDAO payformDAO;
-	
+
+	public Map payForm(Map<String, Integer> pagingMap) throws DataAccessException {
+		int member = pagingMap.get("member");
+		pagingMap.put("member", member);
+		Map payForm = payformDAO.selectMemberInfo(member);
+		return payForm;
+	}
 	
 }
