096680619432bc4e6ad6570146dddd3d308a1768
qingzhi88 <165626294+qingzhi88@users.noreply.github.com>
2024-07-16 18:09:31 +0900

    Update FreeBoardController.java

--- src/main/java/com/isix/easyGym/freeboard/controller/FreeBoardController.java
+++ src/main/java/com/isix/easyGym/freeboard/controller/FreeBoardController.java
@@ -6,9 +6,5 @@ import com.isix.easyGym.freeboard.dto.FreeboardArticleDTO;
 
 
 public interface FreeBoardController {
-    List<FreeboardArticleDTO> getAllArticles();
-    FreeboardArticleDTO getArticleById(int id);
-    void createArticle(FreeboardArticleDTO article);
-    void updateArticle(FreeboardArticleDTO article);
-    void deleteArticle(int id);
+    
 }
