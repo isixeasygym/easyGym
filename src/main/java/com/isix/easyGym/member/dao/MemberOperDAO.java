6ca08c2ea5ad97ce2b4c5c0bee145dcd5ed505c2
qingzhi88 <165626294+qingzhi88@users.noreply.github.com>
2024-07-19 11:25:27 +0900

    Add files via upload

---
+++ src/main/java/com/isix/easyGym/member/dao/MemberOperDAO.java
@@ -0,0 +1,11 @@
+package com.isix.easyGym.member.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository("memberOperDAO")
+public interface MemberOperDAO {
+
+	
+}
---
+++ src/main/java/com/isix/easyGym/member/service/MemberOperService.java
@@ -0,0 +1,17 @@
+package com.isix.easyGym.member.service;
+
+import org.springframework.dao.DataAccessException;
+
+import com.isix.easyGym.member.dto.MemberOperDTO;
+
+public interface MemberOperService {
+
+
+	public void addOperator(MemberOperDTO memberOperDTO) throws DataAccessException;
+			
+	public void delOperMember(String id) throws DataAccessException;
+	
+	public MemberOperDTO login(MemberOperDTO memberDTO) throws DataAccessException;
+
+	public boolean checkId(String memberOperId) throws DataAccessException;
+}
