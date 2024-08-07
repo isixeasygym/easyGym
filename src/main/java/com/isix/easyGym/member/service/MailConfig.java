d77012f79f07db7da5a74ab00ab01d7ed62215f8
kyuhyun-han <khyun5420@gmail.com>
2024-08-07 10:11:39 +0900

    회원가입 이메일 인증 완료

---
+++ src/main/java/com/isix/easyGym/member/service/MailConfig.java
@@ -0,0 +1,39 @@
+package com.isix.easyGym.member.service;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+public class MailConfig {
+
+    @Value("${email.account}")
+    private String emailAccount;
+
+    @Value("${email.password}")
+    private String emailPassword;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(emailAccount);
+        mailSender.setPassword(emailPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
